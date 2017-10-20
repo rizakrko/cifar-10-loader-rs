deb66fb1c07336f2d6a5889e1de5f30da096d11d
29rou <g196010r@icu.ac.jp>
2017-10-21 03:56:00 +0900

    change order

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-pub use self::image_pub::CifarImage;
-pub use self::dataset::CifarDataset;
 mod image;
 mod image_pub;
 mod dataset;
+pub use self::image_pub::CifarImage;
+pub use self::dataset::CifarDataset;
